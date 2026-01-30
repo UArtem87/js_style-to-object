ae4c40794362efc7339e0315775885e153b60f16
Artem <usik887@ukr.net>
2026-01-30 20:56:42 +0200

    renamed variables

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,13 +7,13 @@
  */
 
 function convertToObject(sourceString) {
-  const res = sourceString.split(';');
-  const result = res
+  const styleDeclarations = sourceString.split(';');
+  const styleEntries = styleDeclarations
     .map((el) => el.split(':'))
     .filter(([el1, el2]) => el1.length !== 0 && el2 !== undefined)
     .map(([el1, el2]) => [el1.trim(), el2.trim()]);
 
-  return Object.fromEntries(result);
+  return Object.fromEntries(styleEntries);
 }
 
 module.exports = convertToObject;
